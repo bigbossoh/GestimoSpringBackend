4de45dd060915af21851f234460ecd73b33b4909
bigbossoh <michel.bossoh@outlook.fr>
2022-02-26 15:11:41 +0000

    Fist Commit initial project spring boot

---
+++ src/main/java/com/bzdata/gestimospringbackend/GestimoSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bzdata.gestimospringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestimoSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestimoSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bzdata/gestimospringbackend/GestimoSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzdata.gestimospringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestimoSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
