e9cde5fc34e44fc6e8a1ef7a618a150b02fa094e
zrangonazaire <astairenazaire@gmail.com>
2022-03-04 10:38:58 +0000

    upadate GRUD PAYS

---
+++ src/main/java/com/bzdata/gestimospringbackend/Controllers/PaysController.java
@@ -0,0 +1,70 @@
+package com.bzdata.gestimospringbackend.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.bzdata.gestimospringbackend.Utils.Constants.APP_ROOT;
+
+import java.util.List;
+
+import com.bzdata.gestimospringbackend.DTOs.PaysDto;
+
+import com.bzdata.gestimospringbackend.Services.PaysService;
+@RestController
+@RequestMapping(APP_ROOT + "/pays")
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@SecurityRequirement(name = "gestimoapi")
+public class PaysController {
+    final PaysService paysService;
+    //CREATION ET MODIFICATION D'UN PAYS
+    @PostMapping("/save")
+    @Operation(summary = "Creation et mise à jour d'un Pays", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity<PaysDto>save(@RequestBody PaysDto dto){
+        log.info("We are going to save a new locatire {}", dto);
+        return ResponseEntity.ok(paysService.save(dto)); 
+    }
+    //SUPPRESSION D'UN PAYS
+    @Operation(summary = "Suppression d'un Pays avec lID en paramètre", security = @SecurityRequirement(name = "bearerAuth"))
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean>delete(@PathVariable("id") Long id){
+        log.info("We are going to save a new locatire {}", id);
+        return ResponseEntity.ok(paysService.delete(id));
+    }
+    // TOUT LES PAYS
+    @Operation(summary = "Liste de tous les Pays", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/tout")
+    public ResponseEntity<List<PaysDto>>findAll(){
+        return ResponseEntity.ok(paysService.findAll());
+    }
+    //GET PAYS BY ID
+    @Operation(summary = "Trouver un pays par son ID", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<PaysDto> findByID(@PathVariable("id") Long id) {
+        log.info("Find by ID{}", id);
+        return ResponseEntity.ok(paysService.findById(id));
+    }
+
+    // GET PAYS BY ID
+    @Operation(summary = "Trouver un pays par son nom", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/findByName/{id}")
+    public ResponseEntity<PaysDto> findByName(@PathVariable("id") String nom) {
+        log.info("Find Pays By nom {}", nom);
+        return ResponseEntity.ok(paysService.findByName(nom));
+    }
+}
