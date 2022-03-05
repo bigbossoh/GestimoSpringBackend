80ef306bbb3327499af25974daa23b0723775444
zrangonazaire <astairenazaire@gmail.com>
2022-03-05 11:35:39 +0000

    GS-31 push Quartier for test Bien

---
+++ src/main/java/com/bzdata/gestimospringbackend/validator/QuartierValidator.java
@@ -0,0 +1,27 @@
+package com.bzdata.gestimospringbackend.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bzdata.gestimospringbackend.DTOs.QuartierDto;
+
+import org.springframework.util.StringUtils;
+
+public class QuartierValidator {
+    public static List<String> validate(QuartierDto dto) {
+        List<String> errors = new ArrayList<>();
+
+        if (dto == null) {
+            errors.add("Veuillez renseigner le nom du Quartier");
+            errors.add("Veuillez renseigner l'abréviation de la commune");
+            return errors;
+        }
+        if (!StringUtils.hasLength(dto.getNomQuartier())) {
+            errors.add("Veuillez renseigner le nom du Quartier");
+        }
+        if (!StringUtils.hasLength(dto.getAbrvQuartier())) {
+            errors.add("Veuillez renseigner l'abréviation du Quartier");
+        }
+        return errors;
+    }
+}
