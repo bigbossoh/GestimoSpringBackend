9cdf315dbb8d22914f9e3c4a85845e4535acabb0
Michel BOSSOH <michel.bossoh@outlook.fr>
2022-03-17 19:52:40 +0000

    Starting coding the test

--- src/test/java/com/bzdata/gestimospringbackend/Services/Impl/VillaServiceImplTest.java
+++ src/test/java/com/bzdata/gestimospringbackend/Services/Impl/VillaServiceImplTest.java
@@ -1,10 +1,20 @@
 package com.bzdata.gestimospringbackend.Services.Impl;
 
+import com.bzdata.gestimospringbackend.DTOs.SiteRequestDto;
+import com.bzdata.gestimospringbackend.DTOs.VillaDto;
+import com.bzdata.gestimospringbackend.Services.VillaService;
+
 import org.junit.jupiter.api.Test;
-
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class VillaServiceImplTest {
+   
 
-    @Test
-    void save() {
+    public void shouldSaveVillaCategoryWithSuccess(){
+        
     }
 }
