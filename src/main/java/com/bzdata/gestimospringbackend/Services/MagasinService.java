ffed9352b637877a884a3bdce267ea0d3bbe1ad9
zrangonazaire <astairenazaire@gmail.com>
2022-05-06 12:49:11 +0000

    correct magasin

--- src/main/java/com/bzdata/gestimospringbackend/Services/MagasinService.java
+++ src/main/java/com/bzdata/gestimospringbackend/Services/MagasinService.java
@@ -7,13 +7,21 @@ import java.util.List;
 
 public interface MagasinService {
 
-    MagasinDto save(MagasinDto dto);
+    boolean save(MagasinDto dto);
+
     boolean delete(Long id);
+
     Long maxOfNumBienMagasin();
+
     List<MagasinDto> findAll();
+
     MagasinDto findById(Long id);
+
     MagasinDto findByName(String nom);
+
     List<MagasinDto> findAllBySite(SiteRequestDto siteRequestDto);
+
     List<MagasinDto> findAllByIdSite(Long id);
+
     List<MagasinDto> findAllByIdEtage(Long id);
 }
--- src/main/java/com/bzdata/gestimospringbackend/repository/MagasinRepository.java
+++ src/main/java/com/bzdata/gestimospringbackend/repository/MagasinRepository.java
@@ -2,6 +2,9 @@ package com.bzdata.gestimospringbackend.repository;
 
 import com.bzdata.gestimospringbackend.Models.Magasin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MagasinRepository extends JpaRepository<Magasin,Long> {
+public interface MagasinRepository extends JpaRepository<Magasin, Long> {
+    @Query("SELECT coalesce(max(magas.numBien), 0) FROM Magasin magas")
+    int getMaxNumMagasin();
 }
