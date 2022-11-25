907b6d9f103bd122b43f45b5c1c1e5a795221a0e
zrangonazaire <astairenazaire@gmail.com>
2022-11-25 13:33:24 +0000

    ss

--- src/main/java/com/bzdata/gestimospringbackend/mappers/BailMapperImpl.java
+++ src/main/java/com/bzdata/gestimospringbackend/mappers/BailMapperImpl.java
@@ -1,5 +1,7 @@
 package com.bzdata.gestimospringbackend.mappers;
 
+import java.util.stream.DoubleStream;
+
 import com.bzdata.gestimospringbackend.DTOs.BailAppartementDto;
 import com.bzdata.gestimospringbackend.DTOs.BailMagasinDto;
 import com.bzdata.gestimospringbackend.DTOs.BailVillaDto;
@@ -8,6 +10,7 @@ import com.bzdata.gestimospringbackend.DTOs.MontantLoyerBailDto;
 import com.bzdata.gestimospringbackend.DTOs.OperationDto;
 import com.bzdata.gestimospringbackend.Models.BailLocation;
 import com.bzdata.gestimospringbackend.Models.MontantLoyerBail;
+import com.bzdata.gestimospringbackend.repository.MontantLoyerBailRepository;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -19,43 +22,46 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 public class BailMapperImpl {
-    // BAIL VILLA MAPPER
-    public BailVillaDto fromBailVilla(BailLocation bailLocation) {
-        BailVillaDto bailLocaDto = new BailVillaDto();
-        BeanUtils.copyProperties(bailLocation, bailLocaDto);
-        bailLocaDto.setIdVilla(bailLocation.getBienImmobilierOperation().getId());
-        bailLocaDto.setFullNomLocatire(bailLocation.getUtilisateurOperation().getNom() + " "
-                + bailLocation.getUtilisateurOperation().getPrenom());
-        bailLocaDto.setIdLocataire(bailLocation.getUtilisateurOperation().getId());
-        return bailLocaDto;
-    }
+        final MontantLoyerBailRepository montantLoyerBailRepository;
+        // BAIL VILLA MAPPER
+        public BailVillaDto fromBailVilla(BailLocation bailLocation) {
+                BailVillaDto bailLocaDto = new BailVillaDto();
+                BeanUtils.copyProperties(bailLocation, bailLocaDto);
+                bailLocaDto.setIdVilla(bailLocation.getBienImmobilierOperation().getId());
+                bailLocaDto.setFullNomLocatire(bailLocation.getUtilisateurOperation().getNom() + " "
+                                + bailLocation.getUtilisateurOperation().getPrenom());
+                bailLocaDto.setIdLocataire(bailLocation.getUtilisateurOperation().getId());
+                return bailLocaDto;
+        }
 
-    // BAIL MAGASIN MAPPER
-    public BailMagasinDto fromBailMagasin(BailLocation bailLocation) {
-        BailMagasinDto bailLocaDto = new BailMagasinDto();
-        BeanUtils.copyProperties(bailLocation, bailLocaDto);
+        // BAIL MAGASIN MAPPER
+        public BailMagasinDto fromBailMagasin(BailLocation bailLocation) {
+                BailMagasinDto bailLocaDto = new BailMagasinDto();
+                BeanUtils.copyProperties(bailLocation, bailLocaDto);
 
-        bailLocaDto.setNomLocataire(bailLocation.getUtilisateurOperation().getNom() + " "
-                + bailLocation.getUtilisateurOperation().getPrenom());
-        bailLocaDto.setIdLocataire(bailLocation.getUtilisateurOperation().getId());
-        bailLocaDto.setCodeBien(bailLocation.getBienImmobilierOperation().getCodeAbrvBienImmobilier());
-        return bailLocaDto;
-    }
+                bailLocaDto.setNomLocataire(bailLocation.getUtilisateurOperation().getNom() + " "
+                                + bailLocation.getUtilisateurOperation().getPrenom());
+                bailLocaDto.setIdLocataire(bailLocation.getUtilisateurOperation().getId());
+                bailLocaDto.setCodeBien(bailLocation.getBienImmobilierOperation().getCodeAbrvBienImmobilier());
+                return bailLocaDto;
+        }
 
-    // BAIL MAGASIN MAPPER
-    public BailAppartementDto fromBailAppartement(BailLocation bailLocation) {
-        BailAppartementDto bailLocaDto = new BailAppartementDto();
-        BeanUtils.copyProperties(bailLocation, bailLocaDto);
-        bailLocaDto.setIdBienImmobilier(bailLocation.getBienImmobilierOperation().getId());
-        bailLocaDto.setNomLocataire(bailLocation.getUtilisateurOperation().getNom() + " "
-                + bailLocation.getUtilisateurOperation().getPrenom());
-        bailLocaDto.setIdLocataire(bailLocation.getUtilisateurOperation().getId());
-        bailLocaDto.setCodeBien(bailLocation.getBienImmobilierOperation().getCodeAbrvBienImmobilier());
-        return bailLocaDto;
-    }
+        // BAIL MAGASIN MAPPER
+        public BailAppartementDto fromBailAppartement(BailLocation bailLocation) {
+                BailAppartementDto bailLocaDto = new BailAppartementDto();
+                BeanUtils.copyProperties(bailLocation, bailLocaDto);
+                bailLocaDto.setIdBienImmobilier(bailLocation.getBienImmobilierOperation().getId());
+                bailLocaDto.setNomLocataire(bailLocation.getUtilisateurOperation().getNom() + " "
+                                + bailLocation.getUtilisateurOperation().getPrenom());
+                bailLocaDto.setIdLocataire(bailLocation.getUtilisateurOperation().getId());
+                bailLocaDto.setCodeBien(bailLocation.getBienImmobilierOperation().getCodeAbrvBienImmobilier());
+                return bailLocaDto;
+        }
+
         // BAIL MAGASIN MAPPER
         public OperationDto fromOperation(BailLocation bailLocation) {
                 OperationDto bailLocaDto = new OperationDto();
+
                 BeanUtils.copyProperties(bailLocation, bailLocaDto);
                 bailLocaDto.setIdBienImmobilier(bailLocation.getBienImmobilierOperation().getId());
                 bailLocaDto.setUtilisateurOperation(bailLocation.getUtilisateurOperation().getNom() + " "
@@ -64,6 +70,11 @@ public class BailMapperImpl {
                 bailLocaDto
                                 .setCodeAbrvBienImmobilier(
                                                 bailLocation.getBienImmobilierOperation().getCodeAbrvBienImmobilier());
+                Double montantloyer = montantLoyerBailRepository.findAll().stream()
+                                .filter(bail -> bail.getBailLocation().equals(bailLocation))
+                                .filter(locat -> locat.isStatusLoyer() == true)
+                                .mapToDouble(mon -> mon.getNouveauMontantLoyer()).sum();
+                                           bailLocaDto.setNouveauMontantLoyer(montantloyer);
                 return bailLocaDto;
         }
 
@@ -80,6 +91,6 @@ public class BailMapperImpl {
                 MontantLoyerBailDto montantLoyerBailDto = new MontantLoyerBailDto();
                 BeanUtils.copyProperties(montantLoyerBail, montantLoyerBailDto);
                 return montantLoyerBailDto;
-}
+        }
 
 }
