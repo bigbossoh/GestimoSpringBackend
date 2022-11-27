92b41cce4c293eb71ff74e8ff90198aba93369b5
bigbossoh <michel.bossoh@outlook.fr>
2022-11-27 15:56:08 +0000

    correction of controller import body

--- src/main/java/com/bzdata/gestimospringbackend/Controllers/SuivieDepenseController.java
+++ src/main/java/com/bzdata/gestimospringbackend/Controllers/SuivieDepenseController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bzdata.gestimospringbackend.DTOs.SuivieDepenseDto;
 import com.bzdata.gestimospringbackend.Services.SuivieDepenseService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
