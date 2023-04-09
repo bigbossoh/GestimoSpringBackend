3a74ddbdee8554120351f8c7c7c09bcc4a1778a0
zrangonazaire <astairenazaire@gmail.com>
2023-04-09 21:37:27 +0000

    corriger le changement de mode de paiement

--- src/main/java/com/bzdata/gestimospringbackend/mappers/BailMapperImpl.java
+++ src/main/java/com/bzdata/gestimospringbackend/mappers/BailMapperImpl.java
@@ -1,7 +1,6 @@
 package com.bzdata.gestimospringbackend.mappers;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.bzdata.gestimospringbackend.DTOs.BailAppartementDto;
