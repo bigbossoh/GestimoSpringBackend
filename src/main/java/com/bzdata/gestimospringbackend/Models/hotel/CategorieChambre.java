568b2100da9128f433a6b7ade258fc582b9cb8a7
zrangonazaire <astairenazaire@gmail.com>
2023-12-02 09:39:29 +0000

    feat: reservation up v1

--- src/main/java/com/bzdata/gestimospringbackend/Models/hotel/CategorieChambre.java
+++ src/main/java/com/bzdata/gestimospringbackend/Models/hotel/CategorieChambre.java
@@ -1,14 +1,11 @@
 package com.bzdata.gestimospringbackend.Models.hotel;
 
+import com.bzdata.gestimospringbackend.Models.AbstractEntity;
+import com.bzdata.gestimospringbackend.Models.Appartement;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-
-import com.bzdata.gestimospringbackend.Models.AbstractEntity;
-import com.bzdata.gestimospringbackend.Models.Appartement;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,15 +19,20 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
-
 public class CategorieChambre extends AbstractEntity {
-    String description;
-    String name;
 
-    @OneToMany
-    @JoinColumn(name = "categorieApartement")
-    List<Appartement> appartements;
-    @OneToMany
-    @JoinColumn(name = "categorieChambre")
-    List<PrixParCategorieChambre> prixGategories;
+  String description;
+  String name;
+
+  double price;
+  int nbrDiffJour;
+  double pourcentReduc;
+
+  @OneToMany
+  @JoinColumn(name = "categorieApartement")
+  List<Appartement> appartements;
+
+  @OneToMany
+  @JoinColumn(name = "categorieChambre")
+  List<PrixParCategorieChambre> prixGategories;
 }
